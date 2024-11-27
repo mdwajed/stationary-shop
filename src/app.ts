5b54a60a9c4c12f282b70113684c485b52beff29
mdwajed <awzbpl@gmail.com.com>
2024-11-27 15:05:43 +0600

    final committ

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@
 // // Routes
 // app.use("/api", router);
 // export default app;
-import express, { Application } from 'express';
+import express, { Application, NextFunction } from 'express';
 import cors from 'cors';
 import productRouter from './app/modules/product/product.route';
 import orderRouter from './app/modules/order/order.route';
@@ -33,11 +33,12 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-// app.use((req, res, next) => {
-//   console.log(`Incoming ${req.method} request to ${req.url}`);
-//   console.log('Request body:', req.body);
-//   next();
-// });
+app.use((req, res, next: NextFunction) => {
+  res.send('Welcome');
+  console.log(`Incoming ${req.method} request to ${req.url}`);
+  console.log('Request body:', req.body);
+  next();
+});
 app.use(errorHandler);
 console.log(`Current working directory: ${process.cwd()}`);
 export default app;
